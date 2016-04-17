123c1a7d525b0b5cf98a82fb4c203f34c5f87cc5
Christian Parker <cp@cjparker.us>
2016-04-17 11:27:46 -0600

    port change again

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const path = require('path')
 
 module.exports = (() => {
 
-  let port = process.env.port || 8080
+  let port = process.env.PORT || 5000
 
   let app = express()
   app.use(express.static(path.join(__dirname, '../client')));
