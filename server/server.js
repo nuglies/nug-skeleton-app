1152e62d65b74746f2180471110ea145b20259e4
Christian Parker <cp@cjparker.us>
2016-05-25 13:52:25 -0600

    mongo example

--- server/server.js
+++ server/server.js
@@ -17,40 +17,70 @@ const path = require('path')
 
 module.exports = (() => {
 
-  let port = process.env.PORT || 5000
+    let port = process.env.PORT || 5000
 
-  let app = express()
-  app.use(express.static(path.join(__dirname, '../client')));
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+    let app = express()
+    app.use(express.static(path.join(__dirname, '../client')));
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
 
-  // Additional middleware which will set headers that we need on each request.
-  app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    let db = pmongo('mongodb://localhost/sensorsMongoExample')
 
-    // Disable caching so we'll always get the latest comments.
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-  });
+    // Additional middleware which will set headers that we need on each request.
+    app.use(function(req, res, next) {
+        // Set permissive CORS header - this allows this server to be used only as
+        // an API server in conjunction with something like webpack-dev-server.
+        res.setHeader('Access-Control-Allow-Origin', '*');
 
-  let dashboardHandler = (req,res,next) => {
-    let mockData = [
-      'dashboard item one',
-      'dashboard item two',
-      'dashboard item three'
-    ]
-    res.json(mockData)
-  }
+        // Disable caching so we'll always get the latest comments.
+        res.setHeader('Cache-Control', 'no-cache');
+        next();
+    });
 
-  app.get('/dashboard', dashboardHandler)
+    let dashboardHandler = (req, res, next) => {
+        let mockData = [
+            'dashboard item one',
+            'dashboard item two',
+            'dashboard item three'
+        ]
+        res.json(mockData)
+    }
 
-  app.listen(port, () => {
-    console.log(`listening on ${port}`)
-  })
+    app.post('/sensors', (req, res) => {
+
+        console.log(req.body)
+        db.collection('sensors').insert(req.body)
+            .then(() => {
+                console.log('inserted ok')
+                res.sendStatus(201)
+            })
+            .catch(er => {
+                res.sendStatus(500, {
+                    error: er
+                })
+            })
+
+    })
+
+    app.get('/sensors', (req, res) => {
+        db.collection('sensors').find()
+            .then(results => {
+                res.json(results)
+            })
+            .catch(er => {
+                res.sendStatus(500, {
+                    error: er
+                })
+            })
+    })
+
+    app.get('/dashboard', dashboardHandler)
+
+    app.listen(port, () => {
+        console.log(`listening on ${port}`)
+    })
 
 
 
