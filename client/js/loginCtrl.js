d3c4cee3f22204bc7e98268a3c30d5dd9657e19b
Christian Parker <cp@cjparker.us>
2016-06-01 10:14:10 -0600

    just formatting

--- client/js/loginCtrl.js
+++ client/js/loginCtrl.js
@@ -1,37 +1,32 @@
 // loginCtrl.js
 'use strict';
-angular.module('nug-skeleton-app').controller( 'loginCtrl',['$scope', '$http','auth','$location', 'store', function($scope, $http, auth, $location, store) {
+angular.module('nug-skeleton-app').controller('loginCtrl', ['$scope', '$http', 'auth', '$location', 'store',
+    function($scope, $http, auth, $location, store) {
 
-  $scope.auth = auth;
-  
-  
-  
-  auth.signin({
-     icon: 'http://dopediary.com/wp-content/uploads/2011/01/heaven-og-nugs.jpg'
-  });
-  
-  
-	
+        $scope.auth = auth;
 
-}]);
+        auth.signin({
+            icon: 'http://dopediary.com/wp-content/uploads/2011/01/heaven-og-nugs.jpg'
+        });
+
+    }
+]);
 
 
 
 
 
-nugNgApp.controller( 'logoutCtrl',['$scope', '$http','auth','$location', 'store', function($scope, $http, auth, $location, store) {
+nugNgApp.controller('logoutCtrl', ['$scope', '$http', 'auth', '$location', 'store',
+    function($scope, $http, auth, $location, store) {
 
-  $scope.auth = auth;
-  
-  
-	$scope.logout = function() {
-    auth.signout();
-    store.remove('profile');
-    store.remove('token');
-    $location.path('/login');
-  	}
+        $scope.auth = auth;
 
-}]);
+        $scope.logout = function() {
+            auth.signout();
+            store.remove('profile');
+            store.remove('token');
+            $location.path('/login');
+        }
 
-
-
+    }
+]);
